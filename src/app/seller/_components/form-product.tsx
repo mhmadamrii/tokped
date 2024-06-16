ae327705d341a1191ca592767d07728dbc84af13
mhmadamrii <interceptorghost4@gmail.com>
2024-06-16 23:19:09 +0700

    chore&fix: cannot resolve /Spinner

--- src/app/seller/_components/form-product.tsx
+++ src/app/seller/_components/form-product.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Spinner } from '~/components/Spinner';
+import Spinner from '~/components/Spinner';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { api } from '~/trpc/react';
